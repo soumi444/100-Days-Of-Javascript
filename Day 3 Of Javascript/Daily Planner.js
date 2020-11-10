0e73a6b8a595b4ba97c87d19b322167a413891de
soumi444 <64732287+soumi444@users.noreply.github.com>
2020-11-10 12:03:33 +0530

    Add files via upload

---
+++ Day 3 Of Javascript/Daily Planner.js
@@ -0,0 +1,20 @@
+const message = document.querySelector('.message');
+const text = document.getElementById('text');
+const send = document.getElementById('send');
+const clear = document.getElementById('clear');
+
+//click event
+send.addEventListener('click', deliverMessage);
+
+function deliverMessage() {
+    let textValue = text.value;
+    message.innerHTML = textValue;
+}
+
+//second click event
+clear.addEventListener('click', clearField);
+
+function clearField() {
+    message.innerHTML='';
+    text.value = '';
+}
